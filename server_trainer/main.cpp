f055fddda0c3b2c7ae1fe63690f3428d7057edf7
claridelune <camilaclary@gmail.com>
2024-07-07 17:44:36 -0500

    udp

--- server_trainer/main.cpp
+++ server_trainer/main.cpp
@@ -3,9 +3,22 @@
 #include "client.hpp"
 #include "server.hpp"
 
+#include "../udp/udp.hpp"
+#include "../neural_network/dataHandler.hpp"
+#include "../neural_network/neuralNetwork.hpp"
+
 #include "../shared/logger.hpp"
 #include "../shared/socket.hpp"
 #include "../shared/utils.hpp"
+
+#define IP "127.0.0.1"
+#define UDP_PORT "3490"
+
+#include <iostream>
+
+int currSeq = 0;
+
+std::vector<std::string> data;
 
 ProcessorOpts clientOpts {
     uuid(),
@@ -13,8 +26,42 @@ ProcessorOpts clientOpts {
     4490
 };
 
+bool evaluateDatum(const std::string& datum)
+{
+    if (!checkSum(datum) || (datum[0] != 'd' && datum[0] != 'f'))
+        return false;
+
+    if (datum[0] == 'f')
+        return true;
+
+    int seq = binaryToInt(datum.substr(1, 4));
+    return currSeq == seq;
+}
+
 int main() {
-    Logger* logger = new Logger("ServerTrainer");
+    // Receive Data
+    UDPTalker talker(2, IP, UDP_PORT, &evaluateDatum);
+
+    for (; true; currSeq++)
+    {
+        std::string message;
+        message.push_back('r');
+        message += intToBinary(currSeq);
+
+        int cs = getSum(message);
+        message.push_back(cs);
+
+        auto resp = talker.sendNReceive(message);
+
+        std::cout << "Message: " << resp << std::endl;
+
+        if (resp[0] == 'f')
+            break;
+        
+        data.push_back(resp.substr(5, 10));
+    }
+
+    /* Logger* logger = new Logger("ServerTrainer");
     Socket* socket = new Socket();
     TrainerClient* client = new TrainerClient(socket, clientOpts);
     TrainerServer* server = new TrainerServer(socket);
@@ -29,5 +76,5 @@ int main() {
     delete server;
     delete client;
     delete socket;
-    delete logger;
+    delete logger; */
 };
